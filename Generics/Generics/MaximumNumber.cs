6a4ccc42af789fbdb1475353cfdbd79ef50f9327
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-10 15:16:02 +0530

    initial commit

---
+++ Generics/Generics/MaximumNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    // value as M=25, O=50, K=30
+            Console.WriteLine(MaximumNumber.FindMax("Mango", "Orange", "Kiwi"));
+        }
+    }
+}
